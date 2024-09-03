c8a65a2f16af483232b03cca91248b7c9ca25e91
Eden-Yang <minghao.yang.cn@outlook.com>
2024-09-03 18:04:01 +0800

    function_1

---
+++ chapter_2_function_1/build/CMakeFiles/function_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/function_1.dir/function_1.c.o"
+  "CMakeFiles/function_1.dir/function_1.c.o.d"
+  "function_1"
+  "function_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/function_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ chapter_2_function_1/build/CMakeFiles/function_1.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for function_1.
+# This may be replaced when dependencies are built.
